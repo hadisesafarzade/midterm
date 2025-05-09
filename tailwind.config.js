aa6ddb16e1c0d267071346987fd4d07ea73fa702
Hadise <hadisesafarzade1999@gmail.com>
2025-05-09 22:27:20 +0330

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+ // tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          inter: ['"Inter"', 'sans-serif'],
+          playfair: ['"Playfair Display"', 'serif'],
+          playfairSC: ['"Playfair Display SC"', 'serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
